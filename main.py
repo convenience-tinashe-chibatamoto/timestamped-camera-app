521cc2f10448f9408206ea38dfbe6cc30175ec6b
Convenience  Chibatamoto <conveniencechibatamoto@gmail.com>
2024-05-20 10:05:53 +0200

    Add timestamp to the frame

--- main.py
+++ main.py
@@ -12,4 +12,16 @@ if start:
     while True:
         check, frame = camera.read()
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        
+        # Get current time as a datetime object
+        now = datetime.now()
+
+        # Get day and time add them to the frame
+        cv2.putText(img=frame, text=now.strftime("%A"), org=(30, 80),
+                    fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=3, color=(255, 255, 255),
+                    thickness=2, lineType=cv2.LINE_AA)
+        cv2.putText(img=frame, text=now.strftime("%H:%M:%S"), org=(30, 140),
+                    fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=3, color=(255, 0, 0),
+                    thickness=2, lineType=cv2.LINE_AA)
+
         streamlit_image.image(frame)
