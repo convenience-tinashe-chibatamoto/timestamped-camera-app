e9487b5fcf72d7fff548e86de025a7c94e1f6ec9
Convenience  Chibatamoto <conveniencechibatamoto@gmail.com>
2024-05-20 10:00:44 +0200

    Camera capture and display

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ from datetime import datetime
 
 st.title("Motion Detector")
 start = st.button('Start Camera')
+
+if start:
+    streamlit_image = st.image([])
+    camera = cv2.VideoCapture(0)
+
+    while True:
+        check, frame = camera.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        streamlit_image.image(frame)
